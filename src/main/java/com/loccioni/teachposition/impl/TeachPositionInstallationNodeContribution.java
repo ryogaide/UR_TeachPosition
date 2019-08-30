2a181f890de13b85ec1e7f95ff93bb511b5bd1fc
ryogaide <rindocarp@gmail.com>
2019-08-30 18:05:59 +0200

    ReWrite

--- src/main/java/com/loccioni/teachposition/impl/TeachPositionInstallationNodeContribution.java
+++ src/main/java/com/loccioni/teachposition/impl/TeachPositionInstallationNodeContribution.java
@@ -1,5 +1,22 @@
 package com.loccioni.teachposition.impl;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import javax.swing.JFileChooser;
+import javax.swing.JOptionPane;
+import javax.swing.filechooser.FileFilter;
+import javax.swing.filechooser.FileNameExtensionFilter;
+
 import com.ur.urcap.api.contribution.InstallationNodeContribution;
 import com.ur.urcap.api.contribution.installation.InstallationAPIProvider;
 import com.ur.urcap.api.domain.UserInterfaceAPI;
@@ -14,28 +31,19 @@ import com.ur.urcap.api.domain.value.Pose;
 import com.ur.urcap.api.domain.value.jointposition.JointPositions;
 import com.ur.urcap.api.domain.value.simple.Angle;
 import com.ur.urcap.api.domain.value.simple.Length;
-import com.ur.urcap.api.domain.script.ScriptWriter;
-
-import com.ur.urcap.api.domain.variable.VariableModel;
-import com.ur.urcap.api.domain.variable.Variable;
 
 public class TeachPositionInstallationNodeContribution implements InstallationNodeContribution {
 
 	private static final String POPUPTITLE_KEY = "popuptitle";
 	private static final String DEFAULT_VALUE = "Teach Position";
-	private static final String TEACH_KEY = "Set position";
-	private static final String TEST = "test1";
 	
 	private final TeachPositionInstallationNodeView view;
 	private final KeyboardInputFactory keyboardFactory;
 	private DataModel model;
 	private final InstallationAPIProvider apiProvider;
+	private File file;
+	private String line;
 	
-	//debug
-	private VariableModel varModel;
-	private ScriptWriter Writer;
-	private Variable var;
-
 	public TeachPositionInstallationNodeContribution(InstallationAPIProvider apiProvider, DataModel model, 
 			TeachPositionInstallationNodeView view) {
 		this.keyboardFactory = apiProvider.getUserInterfaceAPI().getUserInteraction().getKeyboardInputFactory();
@@ -45,12 +53,17 @@ public class TeachPositionInstallationNodeContribution implements InstallationNo
 		this.apiProvider = apiProvider;
 	}
 	
-	public void SetPosition(final int selectedRow) {
+	public void SetPosition() {
+		final int selectedRow = view.table.getSelectedRow();
+		if(selectedRow  == -1) {
+			JOptionPane.showMessageDialog(null, "You have to select one row", "Message", JOptionPane.WARNING_MESSAGE);
+			return;
+		}
 		UserInterfaceAPI uiapi = apiProvider.getUserInterfaceAPI();
 		uiapi.getUserInteraction().getUserDefinedRobotPosition(new RobotPositionCallback() {
 			@Override
 			public void onOk(Pose pose, JointPositions jointPositions) {
-				model.set(TEACH_KEY, pose);
+				model.set(view.tableModel.getValueAt(selectedRow, 0).toString(), pose);
 				double[] data_double = pose.toArray(Length.Unit.M ,Angle.Unit.RAD);
 				for(int i=1; i<view.tableModel.getColumnCount(); i++) {
 					view.tableModel.setValueAt(data_double[i-1], selectedRow, i);
@@ -65,7 +78,96 @@ public class TeachPositionInstallationNodeContribution implements InstallationNo
 //		var.getDisplayName();
 //		model.set(TEST, data);
 //		varModel.getAll();
-		System.out.println(var.getDisplayName());
+//		System.out.println(var.getDisplayName());
+	}
+	
+	public void fileRead() {
+		JFileChooser filechooser = new JFileChooser();
+		FileFilter filter = new FileNameExtensionFilter("Position file(.variables)", "variables");
+		filechooser.addChoosableFileFilter(filter);
+		filechooser.setFileFilter(filter);
+		
+		int selected = filechooser.showOpenDialog(null);
+		view.tableModel.deleteAll();
+		if(selected == JFileChooser.APPROVE_OPTION) {
+			file = filechooser.getSelectedFile();
+			if(checkBeforeReadFile(file)) {
+				try {
+					BufferedReader br = new BufferedReader(new FileReader(file));
+					while((line = br.readLine()) != null) {
+						Pattern p = Pattern.compile("=p");	//find the data of position
+						Matcher m = p.matcher(line);
+						if(m.find() != true) continue;
+						String[] dataFromFile = line.split(",\\s*|\\s+|\\=p\\[|\\]");
+						view.tableModel.addRowFromFile(dataFromFile);
+					}
+					br.close();
+				}catch(FileNotFoundException err) {
+					System.out.println(err);
+				}catch(IOException err) {
+					System.out.println(err);
+				}
+			}
+		}
+	}
+	
+	public void fileWrite() {
+		SimpleDateFormat sdf = new SimpleDateFormat("MMddHHmm");
+		Date date = new Date();
+		int index = file.getName().lastIndexOf('.');
+		String filename = file.getName().substring(0,index) + "_"+ sdf.format(date) + "." + file.getName().substring(index+1);
+		
+		int option = JOptionPane.showConfirmDialog(null, "Create new file : " + filename + "?",
+				"Confirm", JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE);
+		if(option == JOptionPane.YES_OPTION) {
+			File newFile = new File(file.getParentFile() + "//" + filename);
+			try {
+				newFile.createNewFile();
+			}catch(IOException e2) {
+				return;
+			}
+			try {
+				BufferedReader br = new BufferedReader(new FileReader(file));
+				BufferedWriter bw = new BufferedWriter(new FileWriter(newFile));
+				int columNum	= view.tableModel.getColumnCount();
+				int RowNum		= view.tableModel.getRowCount();
+				outside : while((line = br.readLine()) != null) {
+					for(int i=0; i<RowNum; i++) {
+						if(line.matches(".*" + view.tableModel.getValueAt(i, 0).toString() + "\\=p\\[.*")) {
+							if(line.matches(".*global.*")) {
+								bw.write("  global ");
+							}
+							bw.write(view.tableModel.getValueAt(i, 0).toString() + "=p[");
+							for(int j=1; j<columNum - 1; j++) {
+								bw.write(view.tableModel.getValueAt(i, j) + ", ");
+							}
+							bw.write(view.tableModel.getValueAt(i, columNum - 1) + "]");
+							bw.newLine();
+							continue outside;
+						}
+					}
+					bw.write(line);
+					bw.newLine();
+				}
+				bw.close();
+				br.close();
+			}catch(IOException e3) {
+				return;
+			}
+			JOptionPane.showMessageDialog(null, "Succeeded", "Message", JOptionPane.INFORMATION_MESSAGE);
+		}else if(option == JOptionPane.NO_OPTION) {
+//			JOptionPane.showMessageDialog(null, "Failed", "Message", JOptionPane.INFORMATION_MESSAGE);
+			return;
+		}
+	}
+	
+	private boolean checkBeforeReadFile(File file){
+		if(file.exists()) {
+			if(file.isFile() && file.canRead()) {
+				return true;
+			}
+		}
+		return false;
 	}
 
 	@Override
--- src/main/java/com/loccioni/teachposition/impl/TeachPositionInstallationNodeView.java
+++ src/main/java/com/loccioni/teachposition/impl/TeachPositionInstallationNodeView.java
@@ -4,14 +4,13 @@ import com.ur.urcap.api.contribution.installation.swing.SwingInstallationNodeVie
 
 import javax.swing.Box;
 import javax.swing.BoxLayout;
-import javax.swing.JOptionPane;
+import javax.swing.ButtonGroup;
 import javax.swing.JPanel;
+import javax.swing.JRadioButton;
 import javax.swing.JTextField;
 import javax.swing.ListSelectionModel;
-import javax.swing.filechooser.FileNameExtensionFilter;
 import javax.swing.JButton;
-import javax.swing.JFileChooser;
-import javax.swing.filechooser.*;
+import javax.swing.JLabel;
 import javax.swing.JTable;
 import javax.swing.JScrollPane;
 
@@ -19,48 +18,48 @@ import java.awt.Component;
 import java.awt.Dimension;
 import java.awt.event.*;
 
-import java.io.File;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-
-import java.util.Date;
-import java.util.regex.Pattern;
-import java.util.regex.Matcher;
-
-import java.text.SimpleDateFormat;
-
 public class TeachPositionInstallationNodeView implements SwingInstallationNodeView<TeachPositionInstallationNodeContribution> {
 
 	private final Style style;
 	private JTextField jTextField = new JTextField();
-	private String line;
 	public TableModel tableModel = new TableModel();
-	private JTable table = new JTable();
-	private File file;
+	public JTable table = new JTable();
 	
 	public TeachPositionInstallationNodeView(Style style) {
 		this.style = style;
 	}
-	//test for git
 
 	@Override
 	public void buildUI(JPanel jPanel, final TeachPositionInstallationNodeContribution installationNode) {
+		jPanel.setLayout(new BoxLayout(jPanel, BoxLayout.Y_AXIS));
+		
 		JPanel panelButton = new JPanel();
 		panelButton.setLayout(new BoxLayout(panelButton, BoxLayout.X_AXIS));
-		panelButton.add(JFileChooser());
+		
+		JPanel panelSelect = new JPanel();
+		ButtonGroup group = new ButtonGroup();
+		JRadioButton UrA = new JRadioButton("UR A (ext)");
+		JRadioButton UrB = new JRadioButton("UR B (int)");
+		group.add(UrA);
+		group.add(UrB);
+		panelSelect.add(UrA);
+		panelSelect.add(UrB);
+		UrA.addActionListener(new RobotSelectListener());
+		UrB.addActionListener(new RobotSelectListener());
+		panelSelect.setLayout(new BoxLayout(panelSelect, BoxLayout.Y_AXIS));
+		panelButton.add(panelSelect);
+		
+		panelButton.add(createHorizontalSpacing());
+		panelButton.add(JFileChooser(installationNode));
 		panelButton.add(createHorizontalSpacing());
 		panelButton.add(JRegister(installationNode));
 		panelButton.add(createHorizontalSpacing());
-		panelButton.add(JFileWriter());
+		panelButton.add(JFileWriter(installationNode));
 		panelButton.add(createHorizontalSpacing());
 		panelButton.add(test(installationNode));
-		
-		jPanel.setLayout(new BoxLayout(jPanel, BoxLayout.Y_AXIS));
+		panelButton.setLayout(new BoxLayout(panelButton, BoxLayout.X_AXIS));
 		jPanel.add(panelButton);
+		
 		jPanel.add(createVerticalSpacing());
 		jPanel.add(varTable());
 	}
@@ -76,50 +75,31 @@ public class TeachPositionInstallationNodeView implements SwingInstallationNodeV
 		return button;
 	}
 	
-	private JButton JFileChooser() {
+	class RobotSelectListener implements ActionListener{
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			JRadioButton button = (JRadioButton) e.getSource();
+			System.out.println(button.getName());
+		}
+	}
+	
+	private JButton JFileChooser(final TeachPositionInstallationNodeContribution installationNode) {
 		JButton button = new JButton("Open");
 		button.addActionListener(new ActionListener(){
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				JFileChooser filechooser = new JFileChooser();
-				FileFilter filter = new FileNameExtensionFilter("Position file(.variables)", "variables");
-				filechooser.addChoosableFileFilter(filter);
-				filechooser.setFileFilter(filter);
-				
-				int selected = filechooser.showOpenDialog(null);
-				tableModel.deleteAll();
-				if(selected == JFileChooser.APPROVE_OPTION) {
-					file = filechooser.getSelectedFile();
-					if(checkBeforeReadFile(file) == false) {
-						JOptionPane.showMessageDialog(null, "File is broken or can't be opend");
-					}else {
-						fileRead(file);
-					}
-				}
+				installationNode.fileRead();
 			}
 		});
 		return button;
 	}
 	
-	private JButton JFileWriter() {
+	private JButton JFileWriter(final TeachPositionInstallationNodeContribution installationNode) {
 		JButton button = new JButton("Save");
 		button.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				SimpleDateFormat sdf = new SimpleDateFormat("MMddHHmm");
-				Date date = new Date();
-				int index = file.getName().lastIndexOf('.');
-				String filename = file.getName().substring(0,index) + "_"+ sdf.format(date) + "." + file.getName().substring(index+1);
-				
-				int option = JOptionPane.showConfirmDialog(null, "Create new file : " + filename + "?",
-						"Confirm", JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE);
-				if(option == JOptionPane.YES_OPTION) {
-					fileWrite(filename);
-					JOptionPane.showMessageDialog(null, "Succeeded", "Message", JOptionPane.INFORMATION_MESSAGE);
-				}else if(option == JOptionPane.NO_OPTION) {
-					JOptionPane.showMessageDialog(null, "Failed", "Message", JOptionPane.INFORMATION_MESSAGE);
-					return;
-				}
+				installationNode.fileWrite();
 			}
 		});
 		return button;
@@ -130,82 +110,10 @@ public class TeachPositionInstallationNodeView implements SwingInstallationNodeV
 		button.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				//Add function to choose position from Robot
-				int selectedRow = table.getSelectedRow();
-				if(selectedRow == -1) {
-					JOptionPane.showMessageDialog(null, "You have to select one row", "Message", JOptionPane.WARNING_MESSAGE);
-					return;
-				}
-				installationNode.SetPosition(selectedRow);
-				
-//				for(int i=1; i<tableModel.getColumnCount(); i++) {
-//					tableModel.setValueAt(i, selectedRow, i);
-//				}
+				installationNode.SetPosition();
 			}
 		});
 		return button;
-	}
-	
-	private void fileWrite(String filename) {
-		File newFile = new File(file.getParentFile() + "//" + filename);
-		try {
-			newFile.createNewFile();
-		}catch(IOException e2) {
-			return;
-		}
-		try {
-			BufferedReader br = new BufferedReader(new FileReader(file));
-			BufferedWriter bw = new BufferedWriter(new FileWriter(newFile));
-			int columNum	= tableModel.getColumnCount();
-			int RowNum		= tableModel.getRowCount();
-			outside : while((line = br.readLine()) != null) {
-				for(int i=0; i<RowNum; i++) {
-					if(line.matches(".*" + tableModel.getValueAt(i, 0).toString() + "\\=p\\[.*")) {
-						if(line.matches(".*global.*")) {
-							bw.write("  global ");
-						}
-						bw.write(tableModel.getValueAt(i, 0).toString() + "=p[");
-						for(int j=1; j<columNum - 1; j++) {
-							bw.write(tableModel.getValueAt(i, j) + ", ");
-						}
-						bw.write(tableModel.getValueAt(i, columNum - 1) + "]");
-						bw.newLine();
-						continue outside;
-					}
-				}
-				bw.write(line);
-				bw.newLine();
-			}
-			bw.close();
-			br.close();
-		}catch(IOException e3) {
-			return;
-		}
-	}
-	
-	private void fileRead(File file) {
-		try {
-			BufferedReader br = new BufferedReader(new FileReader(file));
-			while((line = br.readLine()) != null) {
-				Pattern p = Pattern.compile("=p");	//find the data of position
-				Matcher m = p.matcher(line);
-				if(m.find() != true) continue;
-				String[] dataFromFile = line.split(",\\s*|\\s+|\\=p\\[|\\]");
-				tableModel.addRowFromFile(dataFromFile);
-			}
-			br.close();
-		}catch(FileNotFoundException err) {
-			System.out.println(err);
-		}catch(IOException err) {
-			System.out.println(err);
-		}
-	}
-	
-	private static boolean checkBeforeReadFile(File file) {
-		if(file.exists()) {		//file is exist or not
-			if(file.isFile() && file.canRead()) return true;	//file is readable or not
-		}
-		return false;
 	}
 	
 	private JScrollPane varTable() {
@@ -215,6 +123,20 @@ public class TeachPositionInstallationNodeView implements SwingInstallationNodeV
 		table.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
 		table.getTableHeader().setReorderingAllowed(false);
 		return sp;
+	}
+	
+	private Box createDescription(String desc) {
+		Box box = Box.createHorizontalBox();
+		box.setAlignmentX(Component.LEFT_ALIGNMENT);
+		
+		JLabel label = new JLabel(desc);
+		
+		box.add(label);
+		return box;
+	}
+	
+	private Component createSpacer(int width, int height) {
+		return Box.createRigidArea(new Dimension(width, height));
 	}
 	
 	private Component createHorizontalSpacing() {
